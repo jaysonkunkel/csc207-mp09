6b2aa65a50e93c1138562cfad647abac2238eb49
jaysonkunkel <jayson.kunkel@gmail.com>
2023-11-20 21:16:32 -0600

    JSONInteger.java
    implemented JSONInteger.java

--- JSONInteger.java
+++ JSONInteger.java
@@ -3,6 +3,9 @@ import java.math.BigInteger;
 
 /**
  * JSON integers.
+ * 
+ * @author Jayson Kunkel
+ * @author Sam Bigham
  */
 public class JSONInteger {
 
@@ -55,14 +58,19 @@ public class JSONInteger {
    * Compare to another object.
    */
   public boolean equals(Object other) {
-    return true;        // STUB
+    return (((other instanceof JSONInteger) && (this.value == ((JSONInteger) other).value))
+        || (this.value == other));
   } // equals(Object)
 
   /**
    * Compute the hash code.
    */
   public int hashCode() {
-    return 0;           // STUB
+    if (this.value == null) {
+      return 0;
+    } else {
+      return this.value.hashCode(); 
+    }
   } // hashCode()
 
   // +--------------------+------------------------------------------
@@ -73,7 +81,7 @@ public class JSONInteger {
    * Write the value as JSON.
    */
   public void writeJSON(PrintWriter pen) {
-                        // STUB
+    pen.print(this.toString());
   } // writeJSON(PrintWriter)
 
   /**
