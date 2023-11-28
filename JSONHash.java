a77a9e10687d332175611c2faaaa830420a830f8
jaysonkunkel <jayson.kunkel@gmail.com>
2023-11-27 22:50:53 -0600

    partially implemented JSONHash.java

--- JSONHash.java
+++ JSONHash.java
@@ -1,8 +1,13 @@
 import java.io.PrintWriter;
+import java.math.BigInteger;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.ArrayList;
 
 /**
  * JSON hashes/objects.
+ * @author Jayson Kunkel
+ * @author Sam Bigham
  */
 public class JSONHash {
 
@@ -10,9 +15,28 @@ public class JSONHash {
   // | Fields |
   // +--------+
 
+  //KVPair<JSONString, JSONValue>[] pairs;
+  /**
+   * The underlying array.
+   */
+  ArrayList<KVPair<JSONString, JSONValue>> pairs;
+
+  /**
+   * The number of pairs in the underlying array.
+   */
+  int size = 0;
+
   // +--------------+------------------------------------------------
   // | Constructors |
   // +--------------+
+
+  /**
+   * Create a new hash given the underlying array.
+   */
+  public JSONHash(ArrayList<KVPair<JSONString, JSONValue>>pairs) {
+    this.pairs = pairs;
+    this.size = pairs.size();
+  } // JSONInteger(String)
 
   // +-------------------------+-------------------------------------
   // | Standard object methods |
@@ -22,14 +46,21 @@ public class JSONHash {
    * Convert to a string (e.g., for printing).
    */
   public String toString() {
-    return "";          // STUB
+    return this.pairs.toString();
   } // toString()
 
   /**
    * Compare to another object.
    */
   public boolean equals(Object other) {
-    return true;        // STUB
+   // KVPair[] arr = (other instanceof KVPair[]) // want to convert other to KVPair[] but can't figure it out
+    if (this == null || other == null) { //should it return false if both are null?
+      return false;
+    }
+    // for(int i = 0; i < this.size -1; i++){
+    //   if(this.pairs[i].equals(other[i]))
+    //   }//for
+          
   } // equals(Object)
 
   /**
@@ -47,7 +78,10 @@ public class JSONHash {
    * Write the value as JSON.
    */
   public void writeJSON(PrintWriter pen) {
-                        // STUB
+    //simple implementation for writeJSON... may not work correctly
+    for (int i = 0; i < this.size - 1; i++) {
+      pen.println("key is: " + pairs[i].key()+  " value is : " +pairs[i].value());
+    }//for
   } // writeJSON(PrintWriter)
 
   /**
@@ -64,29 +98,60 @@ public class JSONHash {
   /**
    * Get the value associated with a key.
    */
-  public JSONValue get(JSONString key) {
-    return null;        // STUB
+  public JSONValue get (JSONString key) throws Exception{
+    // int n = key.getValue().hashCode();
+    // return pairs[n].value();
+    for (KVPair<JSONString, JSONValue> pair : this.pairs) {
+      if (pair.key().equals(key)) {
+        return pair.value();
+      } // if
+    } // for
+    throw new Exception("Error: key not found");
   } // get(JSONString)
 
   /**
    * Get all of the key/value pairs.
    */
   public Iterator<KVPair<JSONString,JSONValue>> iterator() {
-    return null;        // STUB
+
+    KVPair<JSONString, JSONValue>[] pairs = this.pairs;
+    int size = this.size;
+    
+    return new Iterator<KVPair<JSONString,JSONValue>>() {
+
+      int i = 0;
+      
+      public boolean hasNext() {
+        return i < size;
+      } // hasNext()
+
+      public KVPair<JSONString,JSONValue> next() throws NoSuchElementException{
+        if (!this.hasNext()) {
+          throw new NoSuchElementException("No elements remaining");
+        }
+        return pairs[i++];
+      } // next()
+    }; // new Iterator
   } // iterator()
 
   /**
    * Set the value associated with a key.
    */
   public void set(JSONString key, JSONValue value) {
-                        // STUB
+    for (int i = 0; i < this.size; i++) {
+      if (this.pairs.get(i).key().equals(key)) {
+        this.pairs.set(i, new KVPair<JSONString,JSONValue>(key, value));
+        return;
+      } // if
+    } // for
+    this.pairs.set(this.size++, new KVPair<JSONString,JSONValue>(key, value));
   } // set(JSONString, JSONValue)
 
   /**
    * Find out how many key/value pairs are in the hash table.
    */
   public int size() {
-    return 0;           // STUB
+    return this.size;
   } // size()
 
 } // class JSONHash
